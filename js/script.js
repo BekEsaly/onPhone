263c61ee5137b00688b4ecf51c6850b134d7ad94
BekEsaly <85221780+BekEsaly@users.noreply.github.com>
2022-08-21 13:43:29 +0600

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const navToggle=document.querySelector(".nav-toggle");
-const menu=document.querySelector(".menu");
-const body=document.querySelector(".body");
-navToggle.addEventListener('click',function onClick(event){
-    navToggle.classList.toggle("change");
-    menu.classList.toggle("active");
-    body.classList.toggle("overlay");
-});
